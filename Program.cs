e00d3f61ef279d593ab1231df2c9ab7f5a8d327d
Ayobola25 <adeboyeayobola@gmail.com>
2022-08-24 22:59:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var database = new AyobolaContext();
+            var student = new Student();
+            student.Name = "joy";
+            student.Martic = 467588;
+            database.Students.Add(student);
+            var final = database.SaveChanges();
+
+
+        }
+    }
+}
+
+
+    
